1ba62556379bf0dea43e023b1804e4bb72320cd1
chriswilder3 <chriswilderdelta19@gmail.com>
2024-12-11 18:21:48 +0530

    added Food Model and added some data

--- mainapp/models.py
+++ mainapp/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Food(models.Model):
+    name = models.CharField( max_length = 100)
+    carbs = models.FloatField() 
+    protien = models.FloatField() 
+    fats = models.FloatField() 
+    calories = models.IntegerField()
+
+    def __str__(self):
+        return f' {self.name, self.calories}'
+
+
